6d3d78a5e4958f4f8d55474a22a640c3f939cc19
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2021-04-04 19:44:14 -0300

    fixed footer position

--- frontend-graphql/src/App.tsx
+++ frontend-graphql/src/App.tsx
@@ -7,14 +7,14 @@ import Router from "./router";
 function App() {
   return (
     <ApolloProvider client={client}>
+      <header className="app-header">
+        <h1>World Teams</h1>
+      </header>
       <div className="app">
-        <header className="app-header">
-          <h1>World Teams</h1>
-        </header>
         <Router />
-        <div className="app-footer">
-          <span>Made with love by Gabriel Quaresma</span>
-        </div>
+      </div>
+      <div className="app-footer">
+        <span>Made with ❤️ by Gabriel Quaresma</span>
       </div>
     </ApolloProvider>
   );
